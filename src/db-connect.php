5fb3db147a28a38cdbb9b877844b1306d6dc5676
minamidate216 <2201412@s.asojuku.ac.jp>
2024-01-10 20:00:02 +0900

    Create db-connect.php

---
+++ src/db-connect.php
@@ -0,0 +1,7 @@
+<?php
+const SERVER = 'mysql220.phy.lolipop.lan';
+const DBNAME = 'LAA1517338-final';
+const USER = 'LAA1517338';
+const PASS = 'date3421';
+$connect = 'mysql:host=' . SERVER . ';dbname=' . DBNAME . ';charset=utf8';
+?>
