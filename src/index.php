37b1c4356d37b435d98b1e774e723c391877573a
minamidate216 <2201412@s.asojuku.ac.jp>
2024-01-10 20:00:07 +0900

    Create index.php

---
+++ src/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8" />
+  <title>商品一覧</title>
+</head>
+<?php require 'db-connect.php'?>
+<body>
+  <h1>商品一覧</h1>
+  <table>
+    <form action="update.php" method="post">
+      <form action="delete.php" method="post">
+        <tr>
+          <th>商品名</th>
+          <th>商品説明</th>
+          <th>材料</th>
+          <th>削除</th>
+        </tr>
+        <?php
+        $pdo = new PDO($connect, USER, PASS);
+        foreach ($pdo->query('select * from Recipes') as $row) {
+          echo '<tr>';
+          echo '<td>', $row['title'], '</td>';
+          echo '<td>', $row['descriptions'], '</td>';
+          echo '<td>', $row['ingredients'], '</td>';
+          echo '<td>','<a href="delete.php?title=',$row['title'],'">削除</a>' , '</td>';
+          echo '</tr>';
+          echo "\n";
+        }
+        ?>
+  </table>
+  <div class="link">
+    <a href="Registration.php">新規登録</a>
+    <a href="update.php">更新</a>
+  </div>
+  </form>
+  </form>
+</body>
+
+</html>
